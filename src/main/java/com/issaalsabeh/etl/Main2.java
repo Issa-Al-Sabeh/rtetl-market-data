151a696244c06f6488351d4747e6deacab0a55c8
IssaAlSabeh <issasab2204@gmail.com>
2026-08-29 16:34:03 +0300

    Fix the test error in PipelineFactoryTest

--- src/main/java/com/issaalsabeh/etl/Main2.java
+++ src/main/java/com/issaalsabeh/etl/Main2.java
@@ -1,16 +1,5 @@
 package com.issaalsabeh.etl;
 
-import com.issaalsabeh.etl.config.SourceConfig;
-import com.issaalsabeh.etl.connector.console.ConsoleSink;
-import com.issaalsabeh.etl.core.Pipeline;
-import com.issaalsabeh.etl.core.PipelineExecutor;
-import com.issaalsabeh.etl.core.Source;
-import com.issaalsabeh.etl.core.factory.SourceFactory;
-import com.issaalsabeh.etl.transformations.PriceNormalizationTransformer;
-import com.issaalsabeh.etl.transformations.ValidationTransformer;
-
-import java.util.Map;
-
 public class Main2 {
 
     public static void main(String[] args) {
